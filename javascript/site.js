d9a81d8e162876ad739999423a26b0651ac0c637
Maximilian Sohrt <max@sohrt.de>
2013-04-29 23:36:02 +0200

    Added jquery-aim menu

--- javascript/site.js
+++ javascript/site.js
@@ -1,6 +1,37 @@
 var site = (function(){
 
+  var $menu;
+
+  function activateMenu(){
+    $menu.menuAim({
+      activate: activateSubmenu,
+      deactivate: deactivateSubmenu
+    });
+  };
+
+  function activateSubmenu(entry) {
+    $(entry).children(".submenu").show();
+    console.log("activate");
+  };
+
+  function deactivateSubmenu(entry) {
+    $(entry).children(".submenu").hide();
+    console.log("deactivate");
+  };
+
+  function init(){
+    $menu = $(".menu");
+    activateMenu();
+  };
+
   return {
     init:init
   }
 })();
+
+$(window).ready(function(){
+  site.init();
+  $(document).click(function() {
+    $(".submenu").hide();
+  });
+});
